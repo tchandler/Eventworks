c9f59f5e34c7a22ec1d339f8957e0a469378dc09
Thomas Chandler <tchandler@monetate.com>
2013-08-24 12:03:13 -0400

    More tests

--- test/EventworksSpec.js
+++ test/EventworksSpec.js
@@ -74,6 +74,60 @@ describe("Eventworks", function() {
 		defaultChannel.unsubscribe('testEvent');
 		defaultChannel.publish('testEvent');
 		expect(fireCount === 4);
+	});
 
+	it("should allow separate named channels", function () {
+		var fireCount = 0;
+
+		//Register event with anonymous callback
+		defaultChannel.subscribe('testEvent', function() {
+			fireCount++;
+		});
+
+		namedChannel.subscribe('testEvent', function() {
+			fireCount++;
+		});
+
+		//Publish only to default channel
+		defaultChannel.publish('testEvent');
+		expect(fireCount === 1);
+
+		//Publish only to named channel
+		namedChannel.publish('testEvent');
+		expect(fireCount === 2);
+	});
+
+	it("should allow isolated Eventworks instances", function() {
+		var Eventworks2 = {};
+		Eventworks.makeEventworks(Eventworks2);
+		expect(Eventworks2.channel);
+		expect(Eventworks2.publish);
+		expect(Eventworks2.subscribe);
+		expect(Eventworks2.unsubscribe);
+
+		var defaultChannel2 = Eventworks2.channel();
+		expect(defaultChannel2 != defaultChannel);
+
+		var namedChannel2 = Eventworks2.channel('namedChannel');
+		expect(namedChannel2 != namedChannel);
+
+		var fireCount = 0;
+
+		//Register event with anonymous callback
+		defaultChannel.subscribe('testEvent', function() {
+			fireCount++;
+		});
+
+		defaultChannel2.subscribe('testEvent', function() {
+			fireCount++;
+		});
+
+		//Publish only to default channel on original instance
+		defaultChannel.publish('testEvent');
+		expect(fireCount === 1);
+
+		//Publish only to default channel on new instance
+		defaultChannel2.publish('testEvent');
+		expect(fireCount === 2);
 	});
 });
