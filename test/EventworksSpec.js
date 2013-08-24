4716d5d1e12e398db8ca47946f34a97f1d29f5c2
Thomas Chandler <tchandler@monetate.com>
2013-08-24 11:22:52 -0400

    Updated tests

--- test/EventworksSpec.js
+++ test/EventworksSpec.js
@@ -18,4 +18,62 @@ describe("Eventworks", function() {
 	it("should create the same channels given the same name", function() {
 		expect(namedChannel === Eventworks.channel("namedChannel"));
 	});
+
+	it("should handle empty strings", function() {
+		expect(defaultChannel === Eventworks.channel(false));
+	});
+
+	it("should fire events correctly", function() {
+		var fireCount = 0;
+		defaultChannel.subscribe('testEvent', function() {
+			fireCount++;
+		});
+
+		defaultChannel.publish('testEvent');
+		expect(fireCount === 1);
+		defaultChannel.publish('anotherEvent');
+		expect(fireCount === 1);
+	});
+
+	it("should set the callback context correctly", function() {
+		var context = {
+			fireCount: 0
+		};
+		defaultChannel.subscribe('testEvent', function() {
+			this.fireCount++;
+		}, context);
+
+		defaultChannel.publish('testEvent');
+		expect(context.fireCount === 1);
+		defaultChannel.publish('anotherEvent');
+		expect(context.fireCount === 1);
+	});
+
+	it("should unsubscribe events correctly", function () {
+		var fireCount = 0;
+		var callback = function() {
+			fireCount += 2;
+		};
+
+		//Register event with anonymous callback
+		defaultChannel.subscribe('testEvent', function() {
+			fireCount++;
+		});
+
+		//Register event with callback that has handle preserved
+		defaultChannel.subscribe('testEvent', callback);
+		defaultChannel.publish('testEvent');
+		expect(fireCount === 3);
+
+		//Only unsubscribe event that's using registered callback
+		defaultChannel.unsubscribe('testEvent', callback);
+		defaultChannel.publish('testEvent');
+		expect(fireCount === 4);
+
+		//Unsubscribe all remaining events
+		defaultChannel.unsubscribe('testEvent');
+		defaultChannel.publish('testEvent');
+		expect(fireCount === 4);
+
+	});
 });
